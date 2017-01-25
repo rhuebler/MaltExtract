53ad494209a0ffc80204cc922b00b9f17d6217ec
Ron Huebler <huebler@shh.mpg.de>
2017-01-25 16:34:31 +0100

    z`xz`d
    Former-commit-id: f1773f0d9d3f35ffe0dbf0d2e74e068a9263c674

--- RMASifterResurgencePrime/src/utility/DataSummeryWriter.java
+++ RMASifterResurgencePrime/src/utility/DataSummeryWriter.java
@@ -1,73 +1,68 @@
 package utility;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import megan.chart.ChartColorManager;
 import megan.core.DataTable;
-import megan.core.Director;
-import megan.core.Document;
 import megan.core.MeganFile;
 import megan.core.SampleAttributeTable;
 import megan.core.SyncArchiveAndDataTable;
 import megan.data.IConnector;
-import megan.rma6.RMA6File;
 
 public class DataSummeryWriter {
 	Logger warning;
-	public static void main(String[] args){
-		writeSummary("/Users/huebler/Desktop/data/","file1","/Users/huebler/Desktop/");
+	public DataSummeryWriter(Logger warning){
+		this.warning = warning;
 	}
-	public static void writeSummary(String directory,String fileName, String outDir) {
+	public void writeSummary(String directory,String fileName, String outDir) {
 		// TODO Auto-generated method stub
 		try {
-		SampleAttributeTable sampleAttributeTable = new SampleAttributeTable();
-		DataTable table = new DataTable();
-		MeganFile file = new MeganFile();
-		File f  = new File(directory+fileName);
-		
-			file.setFileFromExistingFile(f.getCanonicalPath(), true);
-		
-		file.setReadOnly(true);
-		
-		IConnector connector = file.getDataConnector();
-        SyncArchiveAndDataTable.syncArchive2Summary(file.getFileName(), connector, table, sampleAttributeTable);
-      	if(!fileName.endsWith(".rma6"))
-    	   fileName+=".rma6";
-		ChartColorManager chartColorManager = new  ChartColorManager(null);
-		ArrayList<String> output = new ArrayList<String>();
-		output.add("@Creator\t"+ table.getCreator());
-		output.add("@CreationDate\t"+ table.getCreationDate());
-		output.add("@ContentType\t"+table.getContentType());
-		output.add("@Names\t"+fileName);
-		output.add("@BlastMode\t"+table.getBlastMode());
-		output.add("@Uids\t"+table.getSampleUIds()[0]);
-		output.add("@Sizes\t"+table.getSampleSizes()[0]);
-		output.add("@TotalReads\t"+table.getTotalReads());
-		output.add("@AdditionalReads\t"+table.getAdditionalReads());
-		output.add("@Collapse\t"+"Taxonomy\t"+"-1\t");//table.getCollapsed("Taxonomy"));//what are you
-		output.add("@Algorithm\t"+table.getAlgorithm("Taxonomy"));
-		output.add("@NodeStyle\tTaxonomy\tCircle");
-		output.add("@ColorTable\tFews8\tWhite-Green");
-		output.add("@ColorEdits");	
-		for(int key:table.getClass2Counts("Taxonomy").keySet()){
-    	   output.add("TAX\t"+key+"\t"+table.getClass2Counts("Taxonomy").get(key)[0]);
-		}
-		output.add("END_OF_DATA_TABLE");
-		output.add("#SampleID\t" + "@Source\t"+"Size");
-		output.add(fileName+"\t"+file.getFileName()+"\t"+table.getSampleSizes()[0]);
-		Path path = Paths.get(outDir+"MeganSummery/"+fileName+".megan");
-		Files.write(path, output, Charset.forName("UTF-8"));
+			SampleAttributeTable sampleAttributeTable = new SampleAttributeTable();
+			DataTable table = new DataTable();
+			MeganFile file = new MeganFile();
+			File f  = new File(directory+fileName);
+			if(f.canRead() && f.canRead()){
+				file.setFileFromExistingFile(f.getCanonicalPath(), true);
+				file.setReadOnly(true);
+				IConnector connector = file.getDataConnector();
+				SyncArchiveAndDataTable.syncArchive2Summary(file.getFileName(), connector, table, sampleAttributeTable);
+				if(!fileName.endsWith(".rma6"))
+					fileName+=".rma6";
+				ArrayList<String> output = new ArrayList<String>();
+				output.add("@Creator\t"+ table.getCreator());
+				output.add("@CreationDate\t"+ table.getCreationDate());
+				output.add("@ContentType\t"+table.getContentType());
+				output.add("@Names\t"+fileName);
+				output.add("@BlastMode\t"+table.getBlastMode());
+				output.add("@Uids\t"+table.getSampleUIds()[0]);
+				output.add("@Sizes\t"+table.getSampleSizes()[0]);
+				output.add("@TotalReads\t"+table.getTotalReads());
+				output.add("@AdditionalReads\t"+table.getAdditionalReads());
+				output.add("@Collapse\t"+"Taxonomy\t"+"-1\t");//table.getCollapsed("Taxonomy"));//what are you
+				output.add("@Algorithm\t"+table.getAlgorithm("Taxonomy"));
+				output.add("@NodeStyle\tTaxonomy\tCircle");
+				output.add("@ColorTable\tFews8\tWhite-Green");
+				output.add("@ColorEdits");	
+				for(int key:table.getClass2Counts("Taxonomy").keySet()){
+					output.add("TAX\t"+key+"\t"+table.getClass2Counts("Taxonomy").get(key)[0]);
+				}
+				output.add("END_OF_DATA_TABLE");
+				output.add("#SampleID\t" + "@Source\t"+"Size");
+				output.add(fileName+"\t"+file.getFileName()+"\t"+table.getSampleSizes()[0]);
+				Path path = Paths.get(outDir+"/MeganSummaries/"+fileName+".megan");
+				Files.write(path, output, Charset.forName("UTF-8"));
+			}else{
+				warning.log(Level.SEVERE,"File does not exist: "+fileName);
+			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			warning.log(Level.SEVERE,"Cannot Write or Read file",e);
 		}
 	}
 
