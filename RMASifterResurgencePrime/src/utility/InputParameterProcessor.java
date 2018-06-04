101f3710f1763ea5251048ebc49f5dcde77e5ff3
Ron Huebler <huebler@shh.mpg.de>
2018-06-04 16:00:04 +0200

    BugFix
    Former-commit-id: c8b45e9214bfa9b721f84ce8c1a7c2166d378900

--- RMASifterResurgencePrime/src/utility/InputParameterProcessor.java
+++ RMASifterResurgencePrime/src/utility/InputParameterProcessor.java
@@ -373,7 +373,7 @@ public class InputParameterProcessor {
     	        
     	        if(commandLine.hasOption("minPI")){
     	        	double minP = Double.parseDouble(commandLine.getOptionValue("minPI"));
-    	        	if(minP < 1.0){
+    	        	if(minP <= 1.0){
     	        		log.log(Level.WARNING, "Minimum pIdent "+ minP+" smaller than 1.0");
     	        		minP *= 100;
     	        		log.log(Level.WARNING, "Set Minimum pIdent "+ minP);
