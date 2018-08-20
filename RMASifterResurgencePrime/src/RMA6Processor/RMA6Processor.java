476b0eeb721d6d48591237975ef74f0a60c1bd39
Ron Huebler <huebler@shh.mpg.de>
2018-08-20 13:51:59 +0200

    BugFix
    Former-commit-id: 28fc9c15af5c190c309d6e2d0edbe1a427b191e9

--- RMASifterResurgencePrime/src/RMA6Processor/RMA6Processor.java
+++ RMASifterResurgencePrime/src/RMA6Processor/RMA6Processor.java
@@ -160,7 +160,7 @@ public void process() {// processing
 				nodeProcessor.process(inDir, fileName, topPercent, maxLength);
 				results.put(id, nodeProcessor);
 		  }//TaxIDs	
-		log.log(Level.INFO, "Finished File: "+fileName);
+	
 		RMA6OutputProcessor outProcessor = new RMA6OutputProcessor(fileName, outDir,mapReader,warning, behave, alignments, reads);
 		outProcessor.process(results);
 		switch(behave) {
@@ -175,5 +175,6 @@ public void process() {// processing
 				ancientSum = outProcessor.getAncientLine();
 				break;
 		}
+		log.log(Level.INFO, "Finished File: "+fileName);
     }
  }
